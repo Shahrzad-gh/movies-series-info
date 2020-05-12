32b9368c565540a2f873a127cbc9a046df694bf8
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-05-12 17:10:11 +0430

    Updates

---
+++ precache-manifest.5fae0fdd7400865a130284f87f86cbbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905d73867ba0ae726c9f00d57daeb81f",
+    "url": "/movies-series-info/index.html"
+  },
+  {
+    "revision": "d2fcc525f87f6011c089",
+    "url": "/movies-series-info/static/css/main.df13225d.chunk.css"
+  },
+  {
+    "revision": "55e8ca949b2fba7ed741",
+    "url": "/movies-series-info/static/js/2.ac5b8075.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-series-info/static/js/2.ac5b8075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fcc525f87f6011c089",
+    "url": "/movies-series-info/static/js/main.05d64407.chunk.js"
+  },
+  {
+    "revision": "4d0301eee97c96a39d98",
+    "url": "/movies-series-info/static/js/runtime-main.68c799a8.js"
+  }
+]);
